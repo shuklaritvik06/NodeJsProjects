d210c6f15c099c7c77a4e82e04fed268438a6eb2
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-03 13:21:14 +0530

    Create streams.js

---
+++ streams.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+// READ STREAM
+
+const stream = fs.createReadStream('relativePath');
+// const stream = fs.createReadStream('relativePath', { encoding: 'utf8' });
+
+stream.on('data',(data)=>{
+  console.log(data);  // Buffer (chunks of data) would be printed, to print the string use toString() method.
+});
+
+// WRITE STREAM
+
+const stream = fs.createWriteStream('relativePath');
+
+stream.on('data',(data)=>{
+  stream.write("Data to be written");
+});
+
+// PIPING THROUGH STREAMS
+
+readstream.pipe(writestream);  // It will pass the chunks to the writestream and will be written :)
