0b9f20d970d2894daf7d0a3eb015808956956448
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-03 23:57:57 +0530

    Update expressRouter.js

--- expressRouter.js
+++ expressRouter.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
 
+router.get("",()=>{})  // Use router instead of app in the different routes file we create.
+
+module.exports = router // Export
+
+// Now in app.js require the route file and then use it as a middleware.
+
+app.use('scope',routerImport);
