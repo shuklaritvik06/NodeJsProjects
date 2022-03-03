9cb2b19465c32328ddf3be6d72c7af2fb59c526a
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-03 14:48:26 +0530

    Update clients_servers.js

--- clients_servers.js
+++ clients_servers.js
@@ -1,9 +1,39 @@
 // Simple server using http module
 
+//   res.setHeader('Content-Type','text/plain');
+
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req,res)=>{
-  console.log("Request Made");
-  res.write("Hello World!");
+  let path = "./parent";
+  switch(req.url){
+    case '':
+      path+='';
+      break;
+    case '':
+      path+='';
+      break;
+    case '':
+      path+='';
+      break;
+    case '':
+      path+='';   
+      break;
+    case '':
+      res.setHeader('Location','/redirectURL');
+      res.end();
+    default:
+      path+='';
+  }
+  fs.readFile(path,(err,data)=>{
+    if(err){
+      res.end();
+    }
+    else{
+      res.write(data);
+      res.end();
+    }
+  });
 });
 server.listen(8000,'localhost',()=>{
   console.log("listening at port 8000");
