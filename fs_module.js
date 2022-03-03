cd169e69bb814d09f5a009005d2d8a544775db8a
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-03 12:41:39 +0530

    Create fs_module.js

---
+++ fs_module.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+// Read File
+
+fs.readFile('relativePath',(err,data)=>{
+  if(err){
+    console.log(err);
+  }
+  console.log(data);  // Returns the Buffer, we can convert it to string using toString() method.
+});
+
+// Write To File
+
+fs.writeFile('relativePath','data',(err)=>{
+  if(err){
+    console.log(err);
+  }
+  console.log("successfully wrote");
+});
+
+// Make Directory & Delete Directory
+
+if(!fs.existSync('relativePath')){
+  fs.mkdir('relativePath',(err)=>{
+    if(err){
+      console.log(err);
+    }
+    console.log("Successfully created directory");
+  });
+}
+else{
+  fs.rmdir('relativePath',(err)=>{
+    if(err){
+      console.log(err);
+    }
+    console.log("Successfully deleted directory");
+  });
+}
+
+// Delete a File
+
+
+if(fs.existSync('relativeFilePath')){
+  fs.unlink('relativePath',(err)=>{
+    if(err){
+      console.log(err);
+    }
+    console.log("Successfully deleted ");
+  });
+}
