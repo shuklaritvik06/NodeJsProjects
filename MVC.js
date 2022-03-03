4c937b17cacd115318cfeedb08fc42459c95a77f
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-04 00:17:59 +0530

    Create MVC.js

---
+++ MVC.js
@@ -0,0 +1,14 @@
+// MVC -> Model, View , Controller
+// Make our code more modular, reusable and easier to read
+
+// MODEL => CONTROLLER => VIEW
+
+// Make a Controllers folder
+
+// Write the handlers code
+
+// export them
+
+// Require them in the route
+
+// Done
