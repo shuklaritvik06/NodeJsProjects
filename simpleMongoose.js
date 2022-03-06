2f7ede9267a0598945909e0881517fe1e05ced83
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-06 17:37:51 +0530

    Update simpleMongoose.js

--- simpleMongoose.js
+++ simpleMongoose.js
@@ -48,3 +48,9 @@ modal.findById(id).then((result)=>{
   console.error(err)
 })
 
+// MongoDB id consists of :
+
+// a 4-byte value representing the seconds since the Unix epoch,
+// a 3-byte machine identifier,
+// a 2-byte process id, and
+// a 3-byte counter, starting with a random value.
