514f7e7275479581b8590a209771635225e5623e
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-03-17 15:40:07 +0530

    Create authUser.js

---
+++ authUser.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const model = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const formidable = require('express-formidable');
+router.use(formidable());
+router.get("/register", (req, res) => {
+    res.render('register');
+});
+
+// Register a user
+
+router.post("/register", async (req, res) => {
+    const pass = await bcrypt.hash(req.fields.pass,10);
+    const user = new model({
+        username: req.fields.user,
+        email: req.fields.mail,
+        password: pass
+    })
+    user.save().then(()=>res.redirect("/"));
+});
+
+// Login a user
+
+router.post("/login", async (req, res) => {
+    model.find({username: req.fields.name}).then((result)=>{
+        if(result.length===0){
+          res.redirect("/");
+        }
+      try{
+        if(await bcrypt.compare(result[0].password, user.passwordHash)){
+          res.render("secret");
+        }
+        }catch{
+        res.render("Server Error");
+      }
+    });
+});
+
+module.exports = router;
