36cb8da99a771858feb5d344f61edeb1bdd79b39
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-06-09 13:54:27 +0530

    Update eventModule.js

--- eventModule.js
+++ eventModule.js
@@ -7,3 +7,20 @@ myEventEmitter.on('myevent',function(arguement)=>{
 });
 myEventEmitter.emit('myevent','HELLO');
 console.log(myEventEmitter.listeners("myevent"));
+myEventEmitter.eventNames().forEach(eventName=>{
+    console.log(eventName);
+});
+console.log(myEventEmitter.listenerCount("maxListenersExceeded"))
+myEventEmitter.listeners("myevent").forEach(listener=>{
+    console.log(listener.toString());
+});
+myEventEmitter.once("myevent",()=>{
+    console.log("myevent once listened");
+})
+myEventEmitter.prependListener("myevent",()=>{
+    console.log("Listeners");
+}).prependListener("myevent",()=>{
+    console.log("Listeners");
+}).prependListener("myevent",()=>{
+    console.log("Listeners");
+})
