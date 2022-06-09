8068fc0d0686cbe3c288d3f7ae133ddd625e4a92
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-06-09 13:05:51 +0530

    Create os_module.js

---
+++ os_module.js
@@ -0,0 +1,10 @@
+const os = require("os");
+// console.log(os.version());
+console.log(os.homedir());  // It will return the home directory of the user.
+console.log(os.platform()); // It will return the platform of the system.
+console.log(os.arch());    // It will return the architecture of the system.
+console.log(os.uptime()); // It will return the uptime of the system.
+console.log(os.freemem()); // It will return the free memory of the system.
+console.log(os.totalmem()); // It will return the total memory of the system.
+console.log(os.cpus()); // It will return the cpu of the system.
+console.log(os.networkInterfaces()); // It will return the network interfaces of the system.
