a7427f20dae8f8d5c4b8161a89b159d0d746525d
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-06-09 15:06:00 +0530

    Create http_module.js

---
+++ http_module.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const server = new http.Server();
+console.log(http.STATUS_CODES)
+// server.on('request', (req, res) => {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World\n');
+// }).listen(5000, 'localhost');
+// console.log('Server running at http://localhost:5000/');
+const httpServer = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    console.log(req.headers)
+    console.log(req.url);
+    console.log(req.method);
+    res.write("Hello\n");
+    res.end('World\n');
+});
+httpServer.listen(5000, 'localhost');
