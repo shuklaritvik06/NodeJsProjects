ce41452c52f867ae311c767f3781ab280c9cd87a
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-06-26 22:02:43 +0530

    Update shareModel.js

--- Shareongo/models/shareModel.js
+++ Shareongo/models/shareModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://ritvik:2NvqH8W64R0SLNJG@cluster0.5mnc8sn.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected")).catch(err=>console.log(err));
+mongoose.connect("mongodb+srv://ritvik:@cluster0.5mnc8sn.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected")).catch(err=>console.log(err));
 
 const Schema = mongoose.Schema({
     fileName: {
