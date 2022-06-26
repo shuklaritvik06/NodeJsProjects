6256bd44f3582ef7ab139204adca5bb48909ca94
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-06-26 23:51:11 +0530

    Add shareongo

--- Shareongo/controllers/shareController.js
+++ Shareongo/controllers/shareController.js
@@ -1,1 +1,54 @@
+const fs = require("fs");
+const nodeMailer = require("nodemailer");
 
+function getHomePage(req, res) {
+  res.render("home");
+}
+
+function uploadFile(req, res) {
+  const fileName = req.headers["file-name"];
+  req.on("data", (data) => {
+    fs.appendFileSync(__dirname + "/uploads/" + fileName, data);
+  });
+  res.end();
+}
+
+
+function sendEmail(req, res) {
+  const name = req.body.senderName;
+  const email = req.body.senderMail;
+  const url = req.body.fileUrl;
+  const transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: "Your Shareongo File",
+    text: `Hi ${name},\n\nYou have received a file from Shareongo.\n\nClick on the link below to download the file.\n\n${url}\n\nRegards,\nShareongo`,
+  };
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json({
+    message: "Email sent",
+  });
+}
+
+function getFile(req, res) {
+  const file = fs.readFileSync(__dirname + "/uploads/" + req.params.fileName);
+  res.end(file);
+}
+
+module.exports = {
+    getHomePage,
+    uploadFile,
+    sendEmail,
+    getFile
+}
--- Shareongo/routes/fileRoutes.js
+++ Shareongo/routes/fileRoutes.js
@@ -1,70 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const File = require("../models/shareModel");
-const nodeMailer = require("nodemailer");
+const  {getHomePage, uploadFile, sendEmail,getFile} = require("../controllers/shareController");
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
+router.get("/", getHomePage);
 
-router.post("/upload", (req, res) => {
-  const fileName = req.headers["file-name"];
-  if (!fs.existsSync(__dirname + "/uploads")) {
-    fs.mkdir(__dirname + "/uploads/", (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  }
-  req.on("data", (data) => {
-    fs.appendFileSync(__dirname + "/uploads/" + fileName, data);
-  });
-  new File({
-    fileName: fileName,
-    filePath: __dirname + "/uploads/" + fileName,
-  })
-    .save()
-    .then(() => {
-      res.send("File uploaded successfully");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/upload",uploadFile);
 
-router.post("/email", (req, res) => {
-  const name = req.body.senderName;
-  const email = req.body.senderMail;
-  const url = req.body.fileUrl;
-  const transporter = nodeMailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "ritvikshukla261@gmail.com",
-      pass: "",
-    },
-  });
-  const mailOptions = {
-    from: "ritvikshukla261@gmail.com",
-    to: email,
-    subject: "Shareongo",
-    text: `Hi ${name},\n\nYou have received a file from Shareongo.\n\nClick on the link below to download the file.\n\n${url}\n\nRegards,\nShareongo`,
-  };
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  res.json({
-    message: "Email sent",
-  });
-});
+router.post("/email",sendEmail);
 
-router.get("/files/:name",(req,res)=>{
-  console.log(req);
-  const fileName = req.params.name;
-  res.render("download",{ fileUrl: __dirname + "/uploads/" + fileName});
-});
-
+router.get("/files/:fileName", getFile);
 
 module.exports = router;
