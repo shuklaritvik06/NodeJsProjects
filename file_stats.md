bd7f504ac990dd31dc5542bbaaa2d1a6186a2866
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-07-31 03:27:51 +0530

    add learnings

---
+++ file_stats.md
@@ -0,0 +1,20 @@
+## File Stats
+
+- dev: numeric identifier for the device containing the file
+- mode: bit-field describing the file type and mode
+- nlink: number of hard links to the file
+- uid: user id
+- gid: group id
+- size: size of the file in bytes
+- atime: last access time
+- mtime: last modification time
+- ctime: last status change time
+- birthtime: creation time
+- atimeMs: last access time in milliseconds
+- mtimeMs: last modification time in milliseconds
+- ctimeMs: last status change time in milliseconds
+- birthtimeMs: creation time in milliseconds
+- blocks: number of blocks allocated to the file
+- blkSize: file system specific block size
+- rdev: device identifier if the file is a special file
+- ino: inode number of that file
--- fs_module.js
+++ fs_module.js
@@ -1,69 +1,109 @@
-const fs = require('fs');
+// // Read-> 4 , Write-> 2, Execute-> 1
 
-// Read File (Sync)
-fs.readFileSync("fileName","utf8",(err,data)=>{
-   console.log(data);
-   fs.writeFileSync("fileName",data);
-})
+const fs = require("fs");
 
+// // Read File (Sync)
+// fs.readFileSync("fileName","utf8",(err,data)=>{
+//    console.log(data);
+//    fs.writeFileSync("fileName",data);
+// })
 
-// Read File (Async)
+// // Read File (Async)
 
-fs.readFile('relativePath',(err,data)=>{
-  if(err){
+// fs.readFile('relativePath',(err,data)=>{
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log(data);  // Returns the Buffer, we can convert it to string using toString() method.
+// });
+
+// // Write To File
+
+// fs.writeFile('relativePath','data',(err)=>{
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log("successfully wrote");
+// });
+
+// // Make Directory & Delete Directory
+
+// if(!fs.existSync('relativePath')){
+//   fs.mkdir('relativePath',(err)=>{
+//     if(err){
+//       console.log(err);
+//     }
+//     console.log("Successfully created directory");
+//   });
+// }
+// else{
+//   fs.rmdir('relativePath',(err)=>{
+//     if(err){
+//       console.log(err);
+//     }
+//     console.log("Successfully deleted directory");
+//   });
+// }
+
+// // Delete a File
+
+// if(fs.existSync('relativeFilePath')){
+//   fs.unlink('relativePath',(err)=>{
+//     if(err){
+//       console.log(err);
+//     }
+//     console.log("Successfully deleted ");
+//   });
+// }
+
+// // Rename a File
+
+// if(fs.existSync('relativeFilePath')){
+//   fs.rename('oldPath','newPath',(err)=>{
+//     if(err){
+//       console.log(err);
+//     }
+//     console.log("Successfully renamed ");
+//   });
+// }
+
+// Change file permissions
+fs.chmod("fs_module.js", 0777, (err) => {
+  if (err) {
     console.log(err);
   }
-  console.log(data);  // Returns the Buffer, we can convert it to string using toString() method.
+  console.log("Successfully changed permissions");
 });
 
-// Write To File
+// Change file ownership
+fs.chown("fs_module.js", 0000, 0000, () => {
+  console.log("Successfully changed owner");
+});
 
-fs.writeFile('relativePath','data',(err)=>{
-  if(err){
+// Change file stats
+fs.stat("fs_module.js", (err, stats) => {
+  if (err) {
     console.log(err);
   }
-  console.log("successfully wrote");
+  console.log(stats);
+  console.log(stats.isFile());
+  console.log(stats.isDirectory());
 });
 
-// Make Directory & Delete Directory
+// Copy file
+fs.copyFile("fs_module.js", "fs_module_copy.js", (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Successfully copied");
+});
 
-if(!fs.existSync('relativePath')){
-  fs.mkdir('relativePath',(err)=>{
-    if(err){
-      console.log(err);
-    }
-    console.log("Successfully created directory");
-  });
-}
-else{
-  fs.rmdir('relativePath',(err)=>{
-    if(err){
-      console.log(err);
-    }
-    console.log("Successfully deleted directory");
-  });
-}
-
-// Delete a File
-
-
-if(fs.existSync('relativeFilePath')){
-  fs.unlink('relativePath',(err)=>{
-    if(err){
-      console.log(err);
-    }
-    console.log("Successfully deleted ");
-  });
-}
-
-// Rename a File
-
-
-if(fs.existSync('relativeFilePath')){
-  fs.rename('oldPath','newPath',(err)=>{
-    if(err){
-      console.log(err);
-    }
-    console.log("Successfully renamed ");
-  });
-}
+fs.unlinkSync("fs_module_copy.js");
+fs.copyFileSync("fs_module.js", "fs_module_copy.js");
+// Delete file
+fs.unlink("fs_module_copy.js", (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Successfully deleted");
+});
