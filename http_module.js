94cd89d4dbac01c0a643416c04114d9768c10950
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-07-31 03:40:18 +0530

    add learnings

--- http_module.js
+++ http_module.js
@@ -9,9 +9,12 @@ console.log(http.STATUS_CODES)
 const httpServer = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     console.log(req.headers)
+    console.log(req.statusCode);
     console.log(req.url);
     console.log(req.method);
     res.write("Hello\n");
     res.end('World\n');
 });
+console.log(http.STATUS_CODES);
+console.log(http.METHODS);
 httpServer.listen(5000, 'localhost');
