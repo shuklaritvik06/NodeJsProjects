af4b9ceacddabc52653fbb7536a8906adc27b810
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-07-31 04:56:04 +0530

    add learnings

--- os_module.js
+++ os_module.js
@@ -8,3 +8,12 @@ console.log(os.freemem()); // It will return the free memory of the system.
 console.log(os.totalmem()); // It will return the total memory of the system.
 console.log(os.cpus()); // It will return the cpu of the system.
 console.log(os.networkInterfaces()); // It will return the network interfaces of the system.
+console.log(os.tmpdir()); // returns os temporary file directory
+console.log(os.version()); // version of OS
+console.log(os.userInfo()); // returns user info
+console.log(os.uptime()); // returns no. of second of uptime
+console.log(os.type()); // name of OS
+console.log(os.hostname()); // host name of pc
+console.log(os.release()); // kernel release version
+console.log(os.platform());
+console.log(os.loadavg());
